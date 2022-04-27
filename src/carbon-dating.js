b4e9680cfe794d07390f126fafba986c0fc54066
Kirill Loiko <kirill.loiko@gmail.com>
2022-04-27 14:05:21 +0300

    carbon-dating complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
+function dateSample(sampleActivity) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (typeof +sampleActivity === 'number') return +sampleActivity * 22387;
+  return false;
 }
+
 
 module.exports = {
   dateSample
