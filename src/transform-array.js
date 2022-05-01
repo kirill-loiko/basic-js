3ae8df63054d03644b92f108c223c75b9e1921e5
Kirill Loiko <kirill.loiko@gmail.com>
2022-05-01 16:50:52 +0300

    transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let newArr = [];
+
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (i === 0 && (arr[i] === '--discard-prev' || arr[i] === '--double-prev')) {
+      i++;
+    }
+
+    if (i === arr.length - 1 && (arr[i] === '--discard-next' || arr[i] === '--double-next')) {
+      return newArr;
+    }
+
+    if ((arr[i] === '--double-prev' && arr[i - 2] === '--discard-next') || (arr[i] === '--discard-prev' && arr[i - 2] === '--discard-next')) {
+      i++;
+    }
+
+    if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+      newArr.push(arr[i]);
+    }
+
+    if (arr[i] === '--discard-next') {
+      i++;
+    } 
+
+    if (arr[i] === '--double-next') {
+      newArr.push(arr[i + 1]);
+    }
+
+    if (arr[i] === '--discard-prev') {
+      newArr.pop();
+    }
+    
+    if (arr[i] === '--double-prev') {
+      newArr.push(arr[i - 1]);
+    }
+  }
+  return newArr;
 }
 
 module.exports = {
